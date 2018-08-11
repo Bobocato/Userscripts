ef007fa4bd717e06059f6adb8022700d7a249717
Tobias Wentzlaff <tobias@wentzlaff.net>
2018-08-11 05:00:13 +0200

    Add animated display of current anime and episode in fullscreen mode.

--- Animeheaven/AnimeheavenEnhanced.user.js
+++ Animeheaven/AnimeheavenEnhanced.user.js
@@ -7,23 +7,26 @@
 // @grant        none
 // ==/UserScript==
 
-function changeEpisode(url){
-    if(typeof(url) != "undefined"){
+const DISPLAY_TITLE_DURATION = 4;
+const FADE_TITLE_DURATION = 1;
+
+function changeEpisode(url) {
+    if (typeof (url) != "undefined") {
         window.location.href = url;
     } else {
         normalscreen();
     }
 }
 
-function setControlOpacity1(){
+function setControlOpacity1() {
     document.getElementById("controls").style.opacity = "1";
 }
 
-function setControlOpacity0(){
+function setControlOpacity0() {
     document.getElementById("controls").style.opacity = "0";
 }
 
-function fullscreen(){
+function fullscreen() {
     //Set fullscreen to style
     //Video Style
     var videoDiv = document.getElementById("videocon");
@@ -37,14 +40,14 @@ function fullscreen(){
     var controlDiv = document.getElementById("controls");
     controlDiv.style.position = "absolute";
     controlDiv.style.bottom = "15px";
-    let left = (window.innerWidth - controls.clientWidth) /2;
+    let left = (window.innerWidth - controls.clientWidth) / 2;
     controlDiv.style.left = left + "px";
     controlDiv.addEventListener("mouseenter", setControlOpacity1, false);
     controlDiv.addEventListener("mouseleave", setControlOpacity0, false);
     setControlOpacity0();
 }
 
-function normalscreen(){
+function normalscreen() {
     //Reset CSS
     //Video Style
     var videoDiv = document.getElementById("videocon");
@@ -58,60 +61,129 @@ function normalscreen(){
     var controlDiv = document.getElementById("controls");
     controlDiv.style.position = "";
     controlDiv.style.bottom = "";
-    let left = (window.innerWidth - controls.clientWidth) /2;
+    let left = (window.innerWidth - controls.clientWidth) / 2;
     controlDiv.style.left = "";
     controlDiv.style.opacity = "";
     controlDiv.removeEventListener("mouseenter", setControlOpacity1, false);
     controlDiv.removeEventListener("mouseleave", setControlOpacity0, false);
 }
 
-(function() {
+function getName() {
+    let episodeName = document.getElementsByClassName("c")[0].textContent.split("\n")[0];
+    return episodeName;
+}
+
+function showEpisodeTitle(durationInSeconds, fadeDuration) {
+    // retrieve episode name
+    const name = getName();
+    // display episode name
+    let opacity = 0;
+    const containerDiv = document.createElement("div");
+    containerDiv.style.maxWidth = "32%";
+    containerDiv.style.paddingRight = "2em";
+    containerDiv.style.paddingLeft = "2em";
+    containerDiv.style.position = "fixed";
+    containerDiv.style.border = "1px solid #696969";
+    containerDiv.style.zIndex = "99999";
+    containerDiv.style.fontFamily = "sans-serif";
+    containerDiv.style.fontSize = "2.1em";
+    containerDiv.style.textAlign = "center";
+    containerDiv.style.marginTop = "2%";
+    containerDiv.style.marginLeft = "2%";
+    containerDiv.style.borderLeft = "0.5em solid #e81e30";
+    containerDiv.style.background = "linear-gradient(0deg,#333333, #505050)";
+    containerDiv.style.opacity = opacity.toString();
+    const episodeLabel = document.createElement("p");
+    episodeLabel.style.color = "white";
+    const textNode = document.createTextNode(name);
+    episodeLabel.appendChild(textNode);
+    containerDiv.appendChild(episodeLabel);
+    document.body.insertBefore(containerDiv, document.body.childNodes[0]);
+
+    // animate
+    let accumulatedTicks = 0;
+    const tickrate = 5;
+    const maxTickCount = Math.round(fadeDuration * 1000 / tickrate);
+
+    let fadeInTimer = setInterval(() => {
+        accumulatedTicks += tickrate;
+        if (accumulatedTicks >= fadeDuration * 1000) {
+            accumulatedTicks = 0;
+            clearInterval(fadeInTimer);
+            setTimeout(() => {
+                let fadeOutTimer = setInterval(() => {
+                    accumulatedTicks += tickrate;
+                    if (accumulatedTicks >= fadeDuration * 1000) {
+                        accumulatedTicks = 0;
+                        clearInterval(fadeOutTimer);
+                        containerDiv.style.display = "none";
+                    }
+                    opacity -= 1 / maxTickCount;
+                    containerDiv.style.opacity = opacity.toString();
+                }, tickrate);
+            }, durationInSeconds * 1000);
+        }
+        opacity += 1 / maxTickCount;
+        containerDiv.style.opacity = opacity.toString();
+    }, tickrate);
+}
+
+
+(function () {
     'use strict';
     console.log("Jump to Script");
     //Get the needed variables...
     var video = document.getElementById("videodiv");
     var nextLink, prevLink;
     var links = document.getElementsByTagName("A");
-    for(let i =0; i < links.length; i++){
-        if(links[i].rel == "next"){
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].rel == "next") {
             nextLink = links[i].href;
-        } else if(links[i].rel == "prev"){
+        } else if (links[i].rel == "prev") {
             prevLink = links[i].href;
         }
     }
+
     //Was fullscreen used before?
-    if(localStorage.getItem("fullscreen") === "true"){
+    if (localStorage.getItem("fullscreen") === "true") {
         fullscreen();
     }
+
+    //Display episode title
+    if (localStorage.getItem("fullscreen")) {
+        showEpisodeTitle(DISPLAY_TITLE_DURATION, FADE_TITLE_DURATION);
+    }
+
     //Video events
     //At the End
-    video.onended = function() {
+    video.onended = function () {
         changeEpisode(nextLink);
     };
     //Safe volume
-    video.onvolumechange = function() {
+    video.onvolumechange = function () {
         localStorage.setItem("volume", video.volume);
     };
-    //Set volume and playbackrate as soon as video starts playing
-    video.onplaying = function() {
-        if (localStorage.getItem("volume") !== null){
+    //Set volume and playbackrate as soon as video starts playing and display episode title
+    video.onplaying = function () {
+        if (localStorage.getItem("volume") !== null) {
             video.volume = localStorage.getItem("volume");
         }
-        if (localStorage.getItem("playrate") !== null){
+        if (localStorage.getItem("playrate") !== null) {
             video.playbackRate = localStorage.getItem("playrate");
         }
         //Safe Playback Rate
-        video.onratechange = function() {
+        video.onratechange = function () {
             localStorage.setItem("playrate", video.playbackRate);
         };
     };
     document.addEventListener("keydown", keyDownTextField, false);
-    function keyDownTextField(e){
+
+    function keyDownTextField(e) {
         var search = document.getElementsByClassName("searchinput")[0];
-        if(document.activeElement !== search){
-            switch(e.which) {
+        if (document.activeElement !== search) {
+            switch (e.which) {
                 case 86: // "v"
-                    if (localStorage.getItem("fullscreen") === null){
+                    if (localStorage.getItem("fullscreen") === null) {
                         localStorage.setItem("fullscreen", true);
                         fullscreen();
                         console.log("Fullscreen enabled");
@@ -128,30 +200,31 @@ function normalscreen(){
                     changeEpisode(nextLink);
                     break;
                 case 38: // "Arrow Up"
-                    try{
-                        video.volume = (Math.round((video.volume + 0.05)*100))/100;
-                    } catch (err){}
-                    console.log("Volume: " + (video.volume * 100) +"%");
+                    try {
+                        video.volume = (Math.round((video.volume + 0.05) * 100)) / 100;
+                    } catch (err) {}
+                    console.log("Volume: " + (video.volume * 100) + "%");
                     break;
                 case 40: // "Arrow Down"
-                    try{
-                        video.volume = (Math.round((video.volume - 0.05)*100))/100;
-                    } catch (err){}
-                    console.log("Volume: " + (video.volume * 100) +"%");
+                    try {
+                        video.volume = (Math.round((video.volume - 0.05) * 100)) / 100;
+                    } catch (err) {}
+                    console.log("Volume: " + (video.volume * 100) + "%");
                     break;
                 case 37: // "Arrow Left"
                     video.currentTime = video.currentTime - 5;
-                    console.log("Current Time: " +(video.currentTime /60));
+                    console.log("Current Time: " + (video.currentTime / 60));
                     break;
                 case 39: // "Arrow Right"
                     video.currentTime = video.currentTime + 5;
-                    console.log("Current Time: " +(Math.round(video.currentTime /60)));
+                    console.log("Current Time: " + (Math.round(video.currentTime / 60)));
                     break;
                 case 77: // "m"
                     video.currentTime = video.currentTime + 85;
-                    console.log("Current Time: " +(Math.round(video.currentTime /60)));
+                    console.log("Current Time: " + (Math.round(video.currentTime / 60)));
                     break;
-                default: return; // exit this handler for other keys
+                default:
+                    return; // exit this handler for other keys
             }
             e.preventDefault(); // prevent the default action
         } else if (e.which == 32) {
